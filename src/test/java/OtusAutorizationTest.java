5049ee1227ffd550a5cae78a720681153a7ec342
Otus_PC <a_reshkin@inbox.ru>
2025-04-10 21:09:40 +0300

    AutoTests Practice

--- src/test/java/OtusAutorizationTest.java
+++ src/test/java/OtusAutorizationTest.java
@@ -29,9 +29,20 @@ public class OtusAutorizationTest {
     @Test
     public void fill() {
         driver.get("https://otus.home.kartushin.su/training.html");
+        WebElement label = driver.findElement(By.id("textInput"));
+        label.sendKeys("test");
+        label.clear();
+        label.sendKeys("test22");
+
+       // driver.findElement(By.id("textInput")).sendKeys("test");
+       // driver.findElement(By.id("textInput")).clear();
+
         driver.findElement(By.id("textInput")).sendKeys("test");
         List<WebElement> elements = driver.findElements(By.xpath("//html/body/div/div[1]/label"));
         System.out.println(elements.size() + "- количество найденных элементов");
+
+        String text = label.getText();
+        System.out.println(text + "текст в лейбле");
     }
 
     @Test
