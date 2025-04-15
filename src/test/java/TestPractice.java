1b26f44a92aa4a26c77240e8d30647c7e7b8f02c
zhemchuzhnyi <a_reshkin@inbox.ru>
2025-04-15 20:55:12 +0300

    AutoTests Practice

--- src/test/java/TestPractice.java
+++ src/test/java/TestPractice.java
@@ -1,8 +1,5 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -24,7 +21,7 @@ public class TestPractice {
     public void webDriverStart() {
         // Создаем опции для Chrome
         ChromeOptions options = new ChromeOptions();
-        // Раскомментируйте строку ниже, если хотите запускать браузер в headless-режиме
+        // Раскомментировать строку ниже, если нужно запускать браузер в headless-режиме
         // options.addArguments("--headless");
 
         // Инициализируем WebDriver
@@ -53,6 +50,34 @@ public class TestPractice {
 
     @Test
     public void test() {
+        // Добавить Cookie#1 с параметром Otus1 и значением Value1
+        driver.manage().addCookie(new Cookie("Otus1", "Value1"));
+
+        // Добавить Cookie#3 с параметром Otus3 и значением Value3
+        // (добавлять через переменную, переменная должна быть сохранена)
+
+        Cookie cookie = new Cookie("Otus3", "Value3");
+        driver.manage().addCookie(cookie);
+
+        // Добавить Cookie#4 с параметром Otus4 и значением Value4
         driver.manage().addCookie(new Cookie("Otus4", "Value4"));
+
+        // Вывести на экран все Cookies
+        // System.out.println(driver.manage().getCookies());
+        // Вывести на экран Cookie1
+        System.out.println(driver.manage().getCookieNamed("Otus1"));
+
+        // Удалить Cookie#2 по имени куки
+        // driver.manage().deleteCookieNamed("Otus2");
+        // System.out.println(driver.manage().getCookies());
+
+        // Удалить Cookie#3 по переменной Cookie
+        driver.manage().deleteCookie(cookie);
+        System.out.println(driver.manage().getCookies());
+
+        // Удалить все куки, убедиться что их нет
+        driver.manage().deleteAllCookies();
+        Assertions.assertEquals(0, driver.manage().getCookies().size());
+
     }
 }
